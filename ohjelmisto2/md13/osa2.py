e9106934f18ff73d447fc0320c95dcbcc5df2b99
fhros <hanki.wifi@gmail.com>
2024-11-11 00:04:22 +0200

    13

--- ohjelmisto2/md13/osa2.py
+++ ohjelmisto2/md13/osa2.py
@@ -1,28 +1,38 @@
-from flask import Flask, jsonify
-import sqlite3
+from collections import OrderedDict
+from flask import Flask, Response
+import mysql.connector
+import json
 
 app = Flask(__name__)
 
-# Funktio, joka hakee lentokentän tiedot tietokannasta
 def hae_lentokentta(icao):
-    yhteys = sqlite3.connect('airports.db')
-    kursori = yhteys.cursor()
-    kursori.execute("SELECT name, municipality FROM airports WHERE icao_code = ?", (icao,))
-    rivi = kursori.fetchone()
-    yhteys.close()
-    if rivi:
-        return {"ICAO": icao, "Name": rivi[0], "Municipality": rivi[1]}
-    else:
+    try:
+        conn = mysql.connector.connect(
+            user="root",
+            password="ideal",
+            host="localhost",
+            database="flight_game",
+            collation="utf8mb4_general_ci"
+        )
+        cursor = conn.cursor()
+        cursor.execute("SELECT name, municipality FROM airport WHERE ident = %s", (icao,))
+        row = cursor.fetchone()
+        conn.close()
+        if row:
+            return OrderedDict([("ICAO", icao), ("Name", row[0]), ("Municipality", row[1])])
+        else:
+            return None
+    except mysql.connector.Error as e:
+        print("Virhe tietokantayhteydessä:", e)
         return None
 
-# Reitti, joka käsittelee GET-pyynnön
 @app.route('/kenttä/<string:icao>', methods=['GET'])
 def kentta(icao):
     lentokentta = hae_lentokentta(icao)
     if lentokentta:
-        return jsonify(lentokentta)
+        return Response(json.dumps(lentokentta), mimetype='application/json')
     else:
-        return jsonify({"error": "Lentokenttää ei löydy"}), 404
+        return Response(json.dumps({"error": "ei löytyny lentokenttää"}), mimetype='application/json')
 
 if __name__ == '__main__':
     app.run(port=3000)
